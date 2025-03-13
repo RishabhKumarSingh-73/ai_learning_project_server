2edc015212469174d60685397765d40d5cbc239e
RishabhKumarSingh-73 <rishabhkumarsinghb@gmail.com>
2025-03-13 14:08:44 +0530

    addedd procfile

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import json
+import os
 from fastapi import FastAPI
+import uvicorn
 
 from llm_functions.memory_recall_assessment import memory_recall_assessment
 
@@ -9,4 +11,8 @@ app = FastAPI()
 async def get_memory_recall_assessment_questions():
     questions_str = await memory_recall_assessment()
     data = json.loads(questions_str)
-    return data
+    return data 
+
+if __name__ == "__main__":
+    port = int(os.getenv("PORT", 8000))  # Default to 8000 if no PORT is assigned
+    uvicorn.run(app, host="0.0.0.0", port=port)
